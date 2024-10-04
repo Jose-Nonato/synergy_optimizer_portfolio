aa61979ddec7aa0d026ed914406b65802ca8b22e
José Nonato <josnonato161@gmail.com>
2024-10-04 09:47:54 -0300

    feat: formating table

--- front/portfolio-optimizer/src/pages/Dashboard.vue
+++ front/portfolio-optimizer/src/pages/Dashboard.vue
@@ -49,26 +49,26 @@ export default {
       tickers: [],
       cols: [
         { name: 'papel', label: 'Papel', field: 'papel', align: 'left' },
-        { name: 'cotacao', label: 'Cotação', field: 'cotacao', align: 'left', sortable: true },
-        { name: 'pl', label: 'P/L', field: 'pl', align: 'left', sortable: true },
-        { name: 'pvp', label: 'P/VP', field: 'pvp', align: 'left', sortable: true },
-        { name: 'psr', label: 'PSR', field: 'psr', align: 'left', sortable: true },
-        { name: 'dy', label: 'Div. Yield', field: 'dy', align: 'left', sortable: true },
-        { name: 'pa', label: 'P. Ativo', field: 'pa', align: 'left', sortable: true },
-        { name: 'pcg', label: 'P/Cap. Giro', field: 'pcg', align: 'left', sortable: true },
-        { name: 'pebit', label: 'P/EBIT', field: 'pebit', align: 'left', sortable: true },
-        { name: 'pacl', label: 'P/Ativ. Cir. Liq', field: 'pacl', align: 'left', sortable: true },
-        { name: 'evebit', label: 'EV/EBIT', field: 'evebit', align: 'left', sortable: true },
-        { name: 'evebitda', label: 'EV/EBITDA', field: 'evebitda', align: 'left', sortable: true },
-        { name: 'mrgebit', label: 'Mrg Ebit', field: 'mrgebit', align: 'left', sortable: true },
-        { name: 'mrgliq', label: 'Mrg Líq', field: 'mrgliq', align: 'left', sortable: true },
-        { name: 'roic', label: 'ROIC', field: 'roic', align: 'left', sortable: true },
-        { name: 'roe', label: 'ROE', field: 'roe', align: 'left', sortable: true },
-        { name: 'liqc', label: 'Liq. Corr', field: 'liqc', align: 'left', sortable: true },
-        { name: 'liq2m', label: 'Liq. 2 Meses', field: 'liq2m', align: 'left', sortable: true },
-        { name: 'patrliq', label: 'Patrim. Líq.', field: 'patrliq', align: 'left', sortable: true },
-        { name: 'divbpatr', label: 'Dív. Brut/Patrim', field: 'divbpatr', align: 'left', sortable: true },
-        { name: 'c5y', label: 'Cresc. Rec. 5a', field: 'c5y', align: 'left', sortable: true }
+        { name: 'cotacao', label: 'Cotação', field: 'cotacao', align: 'left', sortable: true, format: val => `R$ ${this.format_price(val, 2)}` },
+        { name: 'pl', label: 'P/L', field: 'pl', align: 'left', sortable: true, format: val => `${this.format_price(val, 2)}` },
+        { name: 'pvp', label: 'P/VP', field: 'pvp', align: 'left', sortable: true, format: val => `${this.format_price(val, 2)}` },
+        { name: 'psr', label: 'PSR', field: 'psr', align: 'left', sortable: true, format: val => `${this.format_price(val, 3)}` },
+        { name: 'dy', label: 'Div. Yield', field: 'dy', align: 'left', sortable: true, format: val => `${this.format_percent(val)}` },
+        { name: 'pa', label: 'P. Ativo', field: 'pa', align: 'left', sortable: true, format: val => `${this.format_price(val, 3)}` },
+        { name: 'pcg', label: 'P/Cap. Giro', field: 'pcg', align: 'left', sortable: true, format: val => `R$ ${this.format_price(val, 2)}` },
+        { name: 'pebit', label: 'P/EBIT', field: 'pebit', align: 'left', sortable: true, format: val => `R$ ${this.format_price(val, 2)}` },
+        { name: 'pacl', label: 'P/Ativ. Cir. Liq', field: 'pacl', align: 'left', sortable: true, format: val => `R$ ${this.format_price(val, 2)}` },
+        { name: 'evebit', label: 'EV/EBIT', field: 'evebit', align: 'left', sortable: true, format: val => `R$ ${this.format_price(val, 2)}` },
+        { name: 'evebitda', label: 'EV/EBITDA', field: 'evebitda', align: 'left', sortable: true, format: val => `R$ ${this.format_price(val, 2)}` },
+        { name: 'mrgebit', label: 'Mrg Ebit', field: 'mrgebit', align: 'left', sortable: true, format: val => `${this.format_percent(val)}` },
+        { name: 'mrgliq', label: 'Mrg Líq', field: 'mrgliq', align: 'left', sortable: true, format: val => `${this.format_percent(val)}` },
+        { name: 'roic', label: 'ROIC', field: 'roic', align: 'left', sortable: true, format: val => `${this.format_percent(val)}` },
+        { name: 'roe', label: 'ROE', field: 'roe', align: 'left', sortable: true, format: val => `${this.format_percent(val)}` },
+        { name: 'liqc', label: 'Liq. Corr', field: 'liqc', align: 'left', sortable: true, format: val => `${this.format_price(val, 2)}` },
+        { name: 'liq2m', label: 'Liq. 2 Meses', field: 'liq2m', align: 'left', sortable: true, format: val => `R$ ${this.format_price(val, 2)}` },
+        { name: 'patrliq', label: 'Patrim. Líq.', field: 'patrliq', align: 'left', sortable: true, format: val => `R$ ${this.format_price(val, 2)}` },
+        { name: 'divbpatr', label: 'Dív. Brut/Patrim', field: 'divbpatr', align: 'left', sortable: true, format: val => `${this.format_price(val, 2)}` },
+        { name: 'c5y', label: 'Cresc. Rec. 5a', field: 'c5y', align: 'left', sortable: true, format: val => `${this.format_percent(val)}` }
       ],
       visibleColumns: ['papel', 'cotacao', 'pl', 'pvp', 'psr', 'dy', 'pa', 'pcg', 'pebit', 'pacl', 'evebit', 'evebitda', 'mrgebit', 'mrgliq', 'roic', 'roe', 'liqc', 'liq2m', 'patrliq', 'divbpatr', 'c5y'],
       searchInput: ''
@@ -88,6 +88,13 @@ export default {
         .catch(error => {
           console.error('Erro ao pegar os dados de ticker:', error)
         })
+    },
+    format_price (value, quantity) {
+      const val = value.toFixed(quantity).replace('.', ',')
+      return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+    },
+    format_percent (value) {
+      return `${(value).toFixed(2)}%`
     }
   }
 }
